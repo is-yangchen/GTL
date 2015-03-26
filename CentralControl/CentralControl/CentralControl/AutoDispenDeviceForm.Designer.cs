774cc3aa6a4882984d173f21006ce42036d62960
Yang Chen <sjtuyangchen@gmail.com>
2015-03-26 18:50:57 +0800

    change Centtral features

---
+++ CentralControl/CentralControl/CentralControl/AutoDispenDeviceForm.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace CentralControl
+{
+    partial class AutoDispenDeviceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.s = new System.Windows.Forms.Panel();
+            this.yiJiaZhuListView = new System.Windows.Forms.ListView();
+            this.yiJiaZhuLabel = new System.Windows.Forms.Label();
+            this.dianJi4TextBox = new System.Windows.Forms.TextBox();
+            this.dianJi3TextBox = new System.Windows.Forms.TextBox();
+            this.dianJi2TextBox = new System.Windows.Forms.TextBox();
+            this.dianJi1TextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.autoButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.capacityLabel = new System.Windows.Forms.Label();
+            this.volumeLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.stateComboBox = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.s.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.s);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(602, 362);
+            this.panel1.TabIndex = 0;
+            // 
+            // s
+            // 
+            this.s.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.s.Controls.Add(this.stateComboBox);
+            this.s.Controls.Add(this.yiJiaZhuListView);
+            this.s.Controls.Add(this.yiJiaZhuLabel);
+            this.s.Controls.Add(this.dianJi4TextBox);
+            this.s.Controls.Add(this.dianJi3TextBox);
+            this.s.Controls.Add(this.dianJi2TextBox);
+            this.s.Controls.Add(this.dianJi1TextBox);
+            this.s.Controls.Add(this.label8);
+            this.s.Controls.Add(this.label7);
+            this.s.Controls.Add(this.label6);
+            this.s.Controls.Add(this.label5);
+            this.s.Controls.Add(this.label4);
+            this.s.Controls.Add(this.label3);
+            this.s.Location = new System.Drawing.Point(4, 142);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(595, 215);
+            this.s.TabIndex = 1;
+            // 
+            // yiJiaZhuListView
+            // 
+            this.yiJiaZhuListView.GridLines = true;
+            this.yiJiaZhuListView.Location = new System.Drawing.Point(348, 43);
+            this.yiJiaZhuListView.Name = "yiJiaZhuListView";
+            this.yiJiaZhuListView.Size = new System.Drawing.Size(223, 151);
+            this.yiJiaZhuListView.TabIndex = 12;
+            this.yiJiaZhuListView.UseCompatibleStateImageBehavior = false;
+            this.yiJiaZhuListView.View = System.Windows.Forms.View.Details;
+            // 
+            // yiJiaZhuLabel
+            // 
+            this.yiJiaZhuLabel.AutoSize = true;
+            this.yiJiaZhuLabel.Location = new System.Drawing.Point(348, 16);
+            this.yiJiaZhuLabel.Name = "yiJiaZhuLabel";
+            this.yiJiaZhuLabel.Size = new System.Drawing.Size(41, 12);
+            this.yiJiaZhuLabel.TabIndex = 11;
+            this.yiJiaZhuLabel.Text = "label9";
+            // 
+            // dianJi4TextBox
+            // 
+            this.dianJi4TextBox.Location = new System.Drawing.Point(174, 151);
+            this.dianJi4TextBox.Name = "dianJi4TextBox";
+            this.dianJi4TextBox.Size = new System.Drawing.Size(100, 21);
+            this.dianJi4TextBox.TabIndex = 10;
+            // 
+            // dianJi3TextBox
+            // 
+            this.dianJi3TextBox.Location = new System.Drawing.Point(174, 124);
+            this.dianJi3TextBox.Name = "dianJi3TextBox";
+            this.dianJi3TextBox.Size = new System.Drawing.Size(100, 21);
+            this.dianJi3TextBox.TabIndex = 9;
+            // 
+            // dianJi2TextBox
+            // 
+            this.dianJi2TextBox.Location = new System.Drawing.Point(174, 97);
+            this.dianJi2TextBox.Name = "dianJi2TextBox";
+            this.dianJi2TextBox.Size = new System.Drawing.Size(100, 21);
+            this.dianJi2TextBox.TabIndex = 8;
+            // 
+            // dianJi1TextBox
+            // 
+            this.dianJi1TextBox.Location = new System.Drawing.Point(174, 70);
+            this.dianJi1TextBox.Name = "dianJi1TextBox";
+            this.dianJi1TextBox.Size = new System.Drawing.Size(100, 21);
+            this.dianJi1TextBox.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(68, 155);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 12);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "电机4电流：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 12);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "电机3电流：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 12);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "电机2电流：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "电机1电流：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "状态：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "仪器信息显示：";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.autoButton);
+            this.panel2.Controls.Add(this.stopButton);
+            this.panel2.Controls.Add(this.startButton);
+            this.panel2.Controls.Add(this.resetButton);
+            this.panel2.Controls.Add(this.sendButton);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.capacityLabel);
+            this.panel2.Controls.Add(this.volumeLabel);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(596, 136);
+            this.panel2.TabIndex = 0;
+            // 
+            // autoButton
+            // 
+            this.autoButton.Location = new System.Drawing.Point(349, 100);
+            this.autoButton.Name = "autoButton";
+            this.autoButton.Size = new System.Drawing.Size(75, 23);
+            this.autoButton.TabIndex = 10;
+            this.autoButton.Text = "自动";
+            this.autoButton.UseVisualStyleBackColor = true;
+            this.autoButton.Click += new System.EventHandler(this.autoButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(254, 100);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 9;
+            this.stopButton.Text = "急停";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(161, 100);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 8;
+            this.startButton.Text = "开始";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(71, 100);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "复位";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(315, 51);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 6;
+            this.sendButton.Text = "发送";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // capacityLabel
+            // 
+            this.capacityLabel.AutoSize = true;
+            this.capacityLabel.Location = new System.Drawing.Point(69, 58);
+            this.capacityLabel.Name = "capacityLabel";
+            this.capacityLabel.Size = new System.Drawing.Size(41, 12);
+            this.capacityLabel.TabIndex = 3;
+            this.capacityLabel.Text = "label4";
+            // 
+            // volumeLabel
+            // 
+            this.volumeLabel.AutoSize = true;
+            this.volumeLabel.Location = new System.Drawing.Point(69, 31);
+            this.volumeLabel.Name = "volumeLabel";
+            this.volumeLabel.Size = new System.Drawing.Size(41, 12);
+            this.volumeLabel.TabIndex = 2;
+            this.volumeLabel.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "命令：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "发送：";
+            // 
+            // refreshTimer
+            // 
+            this.refreshTimer.Interval = 1000;
+            this.refreshTimer.Tick += new System.EventHandler(this.refreshTimer_Tick);
+            // 
+            // stateComboBox
+            // 
+            this.stateComboBox.FormattingEnabled = true;
+            this.stateComboBox.Location = new System.Drawing.Point(174, 43);
+            this.stateComboBox.Name = "stateComboBox";
+            this.stateComboBox.Size = new System.Drawing.Size(100, 20);
+            this.stateComboBox.TabIndex = 13;
+            // 
+            // AutoDispenDeviceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 387);
+            this.Controls.Add(this.panel1);
+            this.Name = "AutoDispenDeviceForm";
+            this.Text = "全自动培养基分装仪";
+            this.Load += new System.EventHandler(this.AutoDispenDeviceForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.s.ResumeLayout(false);
+            this.s.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label capacityLabel;
+        private System.Windows.Forms.Label volumeLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Button autoButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Panel s;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox dianJi4TextBox;
+        private System.Windows.Forms.TextBox dianJi3TextBox;
+        private System.Windows.Forms.TextBox dianJi2TextBox;
+        private System.Windows.Forms.TextBox dianJi1TextBox;
+        private System.Windows.Forms.Label yiJiaZhuLabel;
+        private System.Windows.Forms.ListView yiJiaZhuListView;
+        private System.Windows.Forms.Timer refreshTimer;
+        private System.Windows.Forms.ComboBox stateComboBox;
+    }
+}
