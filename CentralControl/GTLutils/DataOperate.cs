fb45f6fbca32d1f7a8edc97a37922134fa2eac7c
is-yangchen <sjtuyangchen@gmail.com>
2015-05-14 21:56:06 +0800

    Revert "update DataOperate Class to get real device type and typecast when read and write variusing System;"
    This reverts commit 525762aa95555ba6c52bceafb518b944620ab6d8.

--- CentralControl/GTLutils/DataOperate.cs
+++ CentralControl/GTLutils/DataOperate.cs
@@ -12,82 +12,24 @@ namespace GTLutils
         {
             DeviceManager devicemanager = DeviceManager.getInstance();
             BaseDevice device = devicemanager.getDevice(code);
-            DeviceType devicetype = device.CurrentDeviceType;
-            Type type;
-            switch (devicetype)
-            {
-                case DeviceType.Analysis:
-                    type = ((MultiTunnelVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Clone:
-                    type = ((CloneSelectionVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Dispen:
-                    type = ((AutoDispenVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Liquid:
-                    type = ((LiquidProcessVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Matrix:
-                    type = ((MatrixSystemVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Storage:
-                    type = ((MicroStorageVirtualDevice)device).GetType();
-                    break;
-                default:
-                    type = device.GetType();
-                    break;
-            }
-            PropertyInfo pi = type.GetProperty(VariableName);
-            FieldInfo fi = type.GetField(VariableName);
+            Type type = device.GetType();
+            PropertyInfo pi = type.GetProperty(VariableName);//   .GetField(VariableName);
             if (pi != null)
                 return pi.GetValue(device, null);
-            if (fi != null)
-                return fi.GetValue(device);
-            if (pi == null && fi == null)
-                Console.WriteLine("找不到变量：" + VariableName);
-            return null;
+            else
+                throw new Exception("找不到变量：" + VariableName);
         }
 
         public static String ReadString(string VariableName, string code)
         {
             DeviceManager devicemanager = DeviceManager.getInstance();
             BaseDevice device = devicemanager.getDevice(code);
-            DeviceType devicetype = device.CurrentDeviceType;
-            Type type;
-            switch (devicetype)
-            {
-                case DeviceType.Analysis:
-                    type = ((MultiTunnelVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Clone:
-                    type = ((CloneSelectionVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Dispen:
-                    type = ((AutoDispenVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Liquid:
-                    type = ((LiquidProcessVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Matrix:
-                    type = ((MatrixSystemVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Storage:
-                    type = ((MicroStorageVirtualDevice)device).GetType();
-                    break;
-                default:
-                    type = device.GetType();
-                    break;
-            }
+            Type type = device.GetType();
             PropertyInfo pi = type.GetProperty(VariableName);
-            FieldInfo fi = type.GetField(VariableName);
             if (pi != null)
                 return (String)pi.GetValue(device, null);
-            if (fi != null)
-                return (String)fi.GetValue(device);
-            if (pi == null && fi == null)
-                Console.WriteLine("找不到变量：" + VariableName);
-            return null;
+            else
+                throw new Exception("找不到变量：" + VariableName);
         }
 
         public static object[] ReadArray(string VariableName, string code)
@@ -110,83 +52,24 @@ namespace GTLutils
         {
             DeviceManager devicemanager = DeviceManager.getInstance();
             BaseDevice device = devicemanager.getDevice(code);
-            DeviceType devicetype = device.CurrentDeviceType;
-            Type type;
-            switch (devicetype)
-            {
-                case DeviceType.Analysis:
-                    type = ((MultiTunnelVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Clone:
-                    type = ((CloneSelectionVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Dispen:
-                    type = ((AutoDispenVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Liquid:
-                    type = ((LiquidProcessVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Matrix:
-                    type = ((MatrixSystemVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Storage:
-                    type = ((MicroStorageVirtualDevice)device).GetType();
-                    break;
-                default:
-                    type = device.GetType();
-                    break;
-            }
+            Type type = device.GetType();
             PropertyInfo pi = type.GetProperty(VariableName);
-            FieldInfo fi = type.GetField(VariableName);
-            if (fi != null)
-            {
-                fi.SetValue(device, Convert.ChangeType((string)value, fi.FieldType));
-            }
-
             if (pi != null)
-                pi.SetValue(device, Convert.ChangeType((string)value, pi.PropertyType), null);
-            if (fi == null && pi == null)
-                Console.WriteLine("找不到变量：" + VariableName);
+                pi.SetValue(device, value, null);
+            else
+                throw new Exception("找不到变量：" + VariableName);
         }
 
         public static void WriteString(string VariableName, string code, String value)
         {
             DeviceManager devicemanager = DeviceManager.getInstance();
             BaseDevice device = devicemanager.getDevice(code);
-            DeviceType devicetype = device.CurrentDeviceType;
-            Type type;
-            switch (devicetype)
-            {
-                case DeviceType.Analysis:
-                    type = ((MultiTunnelVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Clone:
-                    type = ((CloneSelectionVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Dispen:
-                    type = ((AutoDispenVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Liquid:
-                    type = ((LiquidProcessVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Matrix:
-                    type = ((MatrixSystemVirtualDevice)device).GetType();
-                    break;
-                case DeviceType.Storage:
-                    type = ((MicroStorageVirtualDevice)device).GetType();
-                    break;
-                default:
-                    type = device.GetType();
-                    break;
-            }
+            Type type = device.GetType();
             PropertyInfo pi = type.GetProperty(VariableName);
-            FieldInfo fi = type.GetField(VariableName);
-            if (fi != null)
-                fi.SetValue(device, Double.Parse((string)value));
             if (pi != null)
-                pi.SetValue(device, (string)value, null);
-            if (fi == null && pi == null)
-                Console.WriteLine("找不到变量：" + VariableName);
+                pi.SetValue(device, value, null);
+            else
+                throw new Exception("找不到变量：" + VariableName);
         }
 
         public static void WriteArray(string VariableName, string code, object[] value)
